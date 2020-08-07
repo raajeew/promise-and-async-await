e32a00d685f23aa9f8de1943ba789ee8a8ad8acf
Rajeev Ranjan <rajeev.ranjan@hitachivantara.com>
2020-08-08 00:23:57 +0530

    added few more await methods

--- promise.js
+++ promise.js
@@ -1,15 +1,48 @@
-function promiseExample(){
-    return new Promise((resolve, reject)=>{
+function getData(){
+    return new Promise((resolve,reject)=>{
         setTimeout(()=>{
-            if(true){
-                resolve('I am resolved.');
+            if(!false){
+                resolve([
+                    {'key':'Name', 'value':'Rajeev Ranjan'},
+                    {'key':'Name', 'value':'Shaurya Singh'},
+                    {'key':'Name', 'value':'Lucky Singh'}
+                ]);
             }else{
-                reject('I am rejected.');
+                reject('Oops, something is went wrong!');
             }
-        },3000);
+            
+        },2000)
     })
 }
 
-promiseExample().then((res)=>{
-    console.log(res);
-})
+function fetchData(){
+    getData()
+    .then((res)=>{
+        let list='';
+        document.getElementById('loading').style.display='none'
+        console.log('Response from promise: '+ JSON.stringify(res));
+        res.forEach(element => {
+            list += `<li>${element.key}:${element.value}</li>`
+        });
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+}
+
+
+async function printData(){
+    console.log('1 statement');
+    try{
+        const data = await getData();
+        console.log(data);
+        const data1 = await getData();
+        console.log(data1);
+    }
+    catch(err){
+        console.log(err)
+    }
+    
+    console.log('3 statement');
+}
+printData();
